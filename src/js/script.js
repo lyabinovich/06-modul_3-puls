3bc96cfe69fe05399439e7a214552e8aa7ec9de5
Andrey Lyabin <lyabinovich@gmail.com>
2021-09-19 15:48:36 +0300

    tabs with toggle content added

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+
+// script for carousel
+
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
@@ -5,4 +8,29 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></img></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></img></button>',
     });
+
+//  script for tabs
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+//  script for toggle content item
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    
 });
